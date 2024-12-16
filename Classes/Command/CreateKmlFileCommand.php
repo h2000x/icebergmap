d5a49673fd745d367fcb3aa2f35ba4420fb0d0a2
h2000x <hk@freies-radio.de>
2024-12-16 11:23:05 +0100

    The Place for the kmlFile is configure via the extension settings.

--- Classes/Command/CreateKmlFileCommand.php
+++ Classes/Command/CreateKmlFileCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Resource\Folder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Resource\File;
@@ -26,8 +27,9 @@ class CreateKmlFileCommand extends Command
         string $name = null
     )
     {
-        $this->extConf = $this->extensionConfiguration->get('icebergmap');
+
         $this->extensionKey = GeneralUtility::camelCaseToLowerCaseUnderscored(explode('\\', $this::class )[1] );
+        $this->extConf = $this->extensionConfiguration->get($this->extensionKey);
         $varPath = Environment::getVarPath();
         $this->data_path = $varPath . '/' . strtolower($this->extensionKey) . '/';
 
@@ -37,6 +39,7 @@ class CreateKmlFileCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        //@todo Es braucht noch ein Prüfrung ob ein neues KML-File erzeugt werden muss.
         $xmlOutput = $this->kmlFileUtility->renderTemplate();
         $ok = file_put_contents($this->data_path . 'iceberg.tmp.xml', $xmlOutput);
         $this->storeFile($this->data_path . 'iceberg.tmp.xml');
@@ -47,17 +50,29 @@ class CreateKmlFileCommand extends Command
     private function storeFile(string $file_path)
     {
         $storage = $this->storageRepository->getDefaultStorage();
-
+        $kmlSavePath  = $storage->getRootLevelFolder()->getIdentifier() . $this->extConf['kmlSavePath'];
+        $folder = new Folder($storage, $kmlSavePath, $kmlSavePath);
+        $this->checkKmlFolder($folder);
         /** @var File $newFile */
         $newFile = $storage->addFile(
             $file_path,
-            $storage->getRootLevelFolder(),
+            $folder,
             'iceberg.kml',
         );
         
-        echo '$newFile' . "<br>\n";
-        print_r(get_class_methods($newFile));
+//        echo '$newFile' . "<br>\n";
+//        print_r(get_class_methods($newFile));
+//        echo '$storage->getRootLevelFolder()' . "<br>\n";
+//        print_r(get_class_methods($storage->getRootLevelFolder()));
         echo $newFile->getIdentifier()  . "<br>\n";
-         
+    }
+
+    private function checkKmlFolder(Folder $folder)
+    {
+        $kmlSavePath = Environment::getPublicPath() . $folder->getPublicUrl();
+        if (is_dir($kmlSavePath)) {
+            return true;
+        }
+        mkdir($kmlSavePath, 0755);
     }
 }
